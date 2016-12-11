80646edab3ef1de848cca8d45e2577803a7e8d5e
redigast13 <redigast13@gmail.com>
2016-12-12 00:57:38 +0500

    #16 @angular/compiler-cli error: Error encountered resolving symbol values statically. Function calls are not supported

--- source/module.ts
+++ source/module.ts
@@ -1,11 +1,14 @@
 import {Inject, NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-const {NgRedux} = require('ng2-redux');
-
 import {Connect} from './connect';
 import {ConnectArray} from './connect-array';
 import {FormStore} from './form-store';
+import { NgRedux } from 'ng2-redux';
+
+export function formStoreFactory(ngRedux: NgRedux<any>) {
+  return new FormStore(ngRedux)
+}
 
 @NgModule({
   imports: [
@@ -23,7 +26,7 @@ import {FormStore} from './form-store';
   providers: [
     {
       provide: FormStore,
-      useFactory: Inject(NgRedux)(ngRedux => new FormStore(ngRedux)),
+      useFactory: formStoreFactory,
       deps: [NgRedux],
     },
   ]
